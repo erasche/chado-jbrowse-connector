bc723d67636e302388505107aa886542c46ce720
Eric Rasche <rasche.eric@gmail.com>
2016-07-21 13:08:04 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 ./chado-jb-rest-api \
     --db "postgres://postgres:$POSTGRES_PASSWORD@db/postgres?sslmode=disable" \
     --listenAddr "0.0.0.0:8500" \
-    --sitePath "$SITE_PATH"
+    --sitePath "$SITE_PATH" \
+    --jbrowse "$JBROWSE"
