39f86d2b493ad732c9d6e09fad1c538fc087d164
Eric Rasche <rasche.eric@gmail.com>
2016-07-21 11:03:10 +0000

    Parameterize SITE_APTH

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 ./chado-jb-rest-api \
     --db "postgres://postgres:$POSTGRES_PASSWORD@db/postgres?sslmode=disable" \
     --listenAddr "0.0.0.0:8500" \
-    --sitePath "http://localhost:8500"
+    --sitePath "$SITE_PATH"
